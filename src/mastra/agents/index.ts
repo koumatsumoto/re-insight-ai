fdb4465b149901de0b9ddcc6de23fa69f7b98550
kou <kou.matsumoto.jp@gmail.com>
2025-03-15 11:56:14 +0900

    refactor: remove estate advisor and weather features

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,5 +1,4 @@
-import { estateAdvisorAgent } from "./estateAdvisorAgent";
 import { googleNewsAgent } from "./googleNewsAgent";
 import { reinfolibAgent } from "./reinfolibAgent";
 
-export { estateAdvisorAgent, googleNewsAgent, reinfolibAgent };
+export { googleNewsAgent, reinfolibAgent };
